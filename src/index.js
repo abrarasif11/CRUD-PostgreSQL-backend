c711567d162772ff97813901d67622c2c42cea28
abrarasif11 <faasif929@gmail.com>
2025-10-22 20:42:19 +0600

    connect PG to backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h2>Time To Start POSTGRE SQL</h2>");
 });
+
 app.listen(port, () => {
   console.log("Server Is Running on port 5000");
 });
