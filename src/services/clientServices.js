4c5c8f54dd1f20186d81d85f5280a7af0a8fbcf5
abrarasif11 <faasif929@gmail.com>
2025-10-24 18:42:26 +0600

    udpate API

--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -15,3 +15,15 @@ export const createClient = async (clientData) => {
 
   return rows[0];
 };
+
+export const updateClient = async (clientId, clientData) => {
+  const { name, email, job, rate, isactive } = clientData;
+
+  const { rows } = await query(
+    `UPDATE clients_tb SET name = $1, email = $2, job = $3, rate = $4, isactive = $5 
+     WHERE id = $6 RETURNING *`,
+    [name, email, job, rate, isactive, clientId]
+  );
+
+  return rows[0];
+};
